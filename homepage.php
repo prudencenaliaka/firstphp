9d68101c5b8c09356774f254211cc04304c61e7b
prudence naliaka <prudencenaliaka@gmail.com>
2020-08-22 14:56:26 +0300

    add into homepage.php

---
+++ homepage.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>homepage</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/homepage.css" type="text/css">
+  </head>
+  <body id="body2">
+    <header>
+      <div id="div1">
+      <nav>
+        <a href="homepage.php">Home</a>
+        <a href="homepage.php">About Us</a>
+        <a href="bookNow.php">Book Ticket</a>
+        <a href="index.php">Account</a>
+        <a href="homepage.php">Contact Us</a>
+      </nav>
+    </div>
+    </header>
+    <div class="main">
+    <section>
+      <h2>Welcome to Safe-travellers campony....We offer the best services</h2>
+    </section>
+    <div class="slider">
+      <figure>
+          <div class="slide">
+        <img src="images/img4.jpeg" alt="" height="400" width="">
+      </div>
+      <div class="slide">
+        <img src="images/img1.jpg" alt="" height="400" width="">
+      </div>
+      <div class="slide">
+    <img src="images/img2.jpeg" alt="" height="400" width="">
+  </div>
+    </figure>
+    </div>
+    <h3>Why Us???</h3>
+    <div class="div1">
+    <p><span>online services</span><br>
+      As opposed to the years back where one had to physically purchase/book a ticket,our campony is advance as LIPA na MPESA and online booking have been implemented thus one can book a ticket at his comfort.</p>
+    <p><span>Comfort</span><br>
+      We have the most comfortable buses on the kenyan roads with lots of legroom to ensure that you reach your destination fully relaxed.An added advantage is the fact that the seats are adjustable to any angle .
+    </p>
+    <p><span>Amenities</span><br>
+Our buses are fitted with high internet connection WI-FI,and enough charging ports for each individual just besides the seats.
+    </p>
+  </div>
+  <div class="div2">
+    <h4>Offers</h4>
+    <p>For first time booking with us,we give an offer of two-thirds of the total cost</p>
+    <p>For booking of more than three people,we give an offer of 5% for each person</p>
+    <p>The first 50 people with high travel rate are given free retreat to various places in Kenya.</p>
+  </div>
+  <section class="sec1">
+  <h4></h4>
+    <table>
+    <tr>
+      <th>Start-Point</th>
+      <th>Destination</th>
+      <th>Time</th>
+      <th>Cost</th>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Kisumu</td>
+      <td>8 hrs</td>
+      <td>1500</td>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Bungoma</td>
+      <td>9 hrs</td>
+      <td>1600</td>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Malaba</td>
+      <td>12 hrs</td>
+      <td>2500</td>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Kakamega</td>
+      <td>8 hrs</td>
+      <td>1600</td>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Lodwar</td>
+      <td>14 hrs</td>
+      <td>3000</td>
+    </tr>
+    <tr>
+      <td>Nairobi</td>
+      <td>Mombasa</td>
+      <td>8 hrs</td>
+      <td>1500</td>
+    </tr>
+  </table>
+</section>
+<div class="div5">
+<h5>Safari-travellers makes bus ticket booking simpler by giving visibility to precise seats that are available and the seats that have been booked. It aggregates inventory of seats of multiple operators to make it easier for the traveler to choose between the bus operators. Our continuously improving technology keeps travelers updated even on the move while frequent travelers benefit by getting solid discounts as well as terrific offers.</h5>
+</div>
+<h1 id="book"><a href=bookNow.php style="color:white">Click here to book your ticket</a></h1>
+</div>
+<footer >
+  <div class="div7">
+<h1 class="hed1">Contact Us</h1><br>
+<span class="span1">P.O.BOX 1345</span><br>
+<span class="span1">0711111111 ,0722222222 ,0733333333</span><br>
+<span class="span1">Email : Stravellers@gmail.com</span>
+</div>
+  <div class="footer-copyright text-center py-3" style="color:hotpink;">© 2020 Copyright:
+  </div>
+</footer>
+  </body>
+</html>
