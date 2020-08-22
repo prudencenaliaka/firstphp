655570ae715da5ccbd8f3209d29008b911575358
prudence naliaka <prudencenaliaka@gmail.com>
2020-08-22 21:11:51 +0300

    add connect.php file

---
+++ connect.php
@@ -0,0 +1,58 @@
+<?php
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$passwordRepeat = $_POST['passwordRepeat'];
+  if(!empty($username) || !empty($email) || !empty($password) || !empty($passwordRepeat)){
+    $servername = 'localhost';
+    $dBUsername = 'root';
+    $dBPassword = 'nalika*1';
+    $dBName = 'signup';
+    $conn =mysqli_connect($servername,$dBUsername,$dBPassword,$dBName);
+  }
+  else {
+    header("location: ../firstphp/index.php?erroremptyfieldsusername.".$username."&email".$email."&password".$password."&passwordRepeat".$passwordRepeat);
+    echo"Please fill the empty fields";
+    exit();
+  }
+    if(!$conn){
+    die("connection failed".mysqli_connect_error());
+    }
+    else {
+        $sql = "INSERT INTO users (username,email,password,passwordRepeat) values ('[$username]','[$email]','[$password]','[$passwordRepeat]')";
+        if($conn->query($sql)=== TRUE){
+      //echo"<script>alert("Insert was succsessfuly recorded")</script>;"
+          header("location: ../firstphp/index.html?succsessfulyrecorded");
+          exit();
+        }
+        else{
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        if($password !== ($passwordRepeat)){
+          header("location: ../firstphp/index.php?errorpasswordcheckusername.".$username."&email".$email);
+          exit();
+        }
+        else if (!filter_var($email , FILTER_VALIDATE_EMAIL)&& !preg_match("/^(a-ZA-ZO-Z9)*$/", $username)) {
+          header("location: ../firstphp/index.php?errorinvalidemailusername.");
+          exit();
+        }
+        else if (!filter_var($email , FILTER_VALIDATE_EMAIL)){
+            header("location: ../firstphp/index.php?errorinvalidemailusername.");
+            exit();
+        }
+        else if(!preg_match("/^(a-ZA-ZO-Z9)*$/", $username)) {
+          header("location: ../firstphp/index.php?errorinvalidemailusername.");
+          exit();
+        }
+        else {
+          $sql="SELECT username FROM users WHERE username=?";
+          $stmt= mysqli_stmt_init($conn);
+          if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("location: ../firstphp/index.php?=errorsqlerror");
+            exit();
+          }
+        }
+        $conn->close();
+}
+
+?>
