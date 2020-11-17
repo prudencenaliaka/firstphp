d396ea881bf4800a88050eb4db5e2143a0bb5eae
prudence naliaka <prudencenaliaka@gmail.com>
2020-11-17 22:59:57 +0300

    added js file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+for (i=0; i<document.test.seat.length; i++){
+if (document.test.seat[i].checked !=true)
+{
+    document.test.seat[i].disabled='true';
+}
+else{
+  document.test.seat[i].disabled='true';
+}
+}
+/*for (i=0; i<document.test.seat.length; i++){
+    if (document.test.seat[i].disabled ==true)
+      document.test.seat[i].disabled='false';
+    }*/
